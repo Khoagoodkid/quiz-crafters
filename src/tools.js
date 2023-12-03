8797de03598d29d475d03b69114cf4531bb90828
Khoagoodkid <haphamanhkhoa@gmail.com>
2023-12-03 17:11:03 -0600

    change env var

--- src/tools.js
+++ src/tools.js
@@ -1,2 +1,2 @@
-export const dbUrl = import.meta.env.VITE_SERVER
-export const usersUrl = import.meta.env.VITE_SERVER + "users/"
+export const dbUrl = import.meta.env.VITE_SERVER_API
+export const usersUrl = import.meta.env.VITE_SERVER_API + "users/"
